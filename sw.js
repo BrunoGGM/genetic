bc0937688c5160351d87f83eef5b69e556e3dcb3
Bruno Giovanni Gomez <br1_gio@hotmail.com>
2018-08-22 18:44:16 -0500

    sw

--- sw.js
+++ sw.js
@@ -77,7 +77,7 @@ window.addEventListener('beforeinstallprompt', function(e) {
 	});
   });
 
-  var deferredPrompt;
+var deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', function(e) {
   console.log('beforeinstallprompt Event fired');
@@ -89,26 +89,4 @@ window.addEventListener('beforeinstallprompt', function(e) {
   return false;
 });
 
-btnSave.addEventListener('click', function() {
-  if(deferredPrompt !== undefined) {
-    // The user has had a postive interaction with our app and Chrome
-    // has tried to prompt previously, so let's show the prompt.
-    deferredPrompt.prompt();
 
-    // Follow what the user has done with the prompt.
-    deferredPrompt.userChoice.then(function(choiceResult) {
-
-      console.log(choiceResult.outcome);
-
-      if(choiceResult.outcome == 'dismissed') {
-        console.log('User cancelled home screen install');
-      }
-      else {
-        console.log('User added to home screen');
-      }
-
-      // We no longer need the prompt.  Clear it up.
-      deferredPrompt = null;
-    });
-  }
-});
